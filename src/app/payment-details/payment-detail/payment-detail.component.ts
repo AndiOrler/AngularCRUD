db796683f3e5e0b42db30a030f15abf8b72ad4ef
AndiOrler <andreas.orler@gmail.com>
2019-09-03 18:55:26 +0200

    first commit

---
+++ src/app/payment-details/payment-detail/payment-detail.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+import { NgForm } from '@angular/forms';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-payment-detail',
+  templateUrl: './payment-detail.component.html',
+  styles: []
+})
+export class PaymentDetailComponent implements OnInit {
+
+  constructor(private service: PaymentDetailService) { }
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+  resetForm(form?: NgForm) {
+    if (form != null) {
+      form.resetForm();
+    }
+    this.service.formData = {
+      PMId: 0,
+      CardOwnerName: '',
+      CardNumber: '',
+      ExpirationDate: '',
+      CVV: ''
+    };
+  }
+
+}
